05b5c7ed3639356b5443ff71acb6d15ac334f45e
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-28 08:41:06 +0500

    Update 255.cpp

--- 255.cpp
+++ 255.cpp
@@ -8,7 +8,7 @@ int main()
 	ifstream in("in.txt");
 	ofstream out("out.txt");
 	int N;
-	cin >> N;
+	in >> N;
 	int a = 1;
 	for (int i = 2; a <= sqrt(N); i++)
 		if (N % i == 0)
@@ -16,8 +16,8 @@ int main()
 			for (a <= sqrt(N); a++;)
 				if (N % a == 0)
 					break;
-			cout << N / a << " " << N - (N / a);
+			out << N / a << " " << N - (N / a);
 			return 0;
 		}
-	cout << 1 << " " << N - 1;
+	out << 1 << " " << N - 1;
 }
